75465977b46cba905080582c22391a9702ed7e0a
TrKienn <92080373+kien15042003@users.noreply.github.com>
2021-11-26 16:52:00 +0700

    Update config.js

--- config.js
+++ config.js
@@ -12,9 +12,9 @@ const CONFIG = {
     btnNo: 'Không nha :3',
     question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
     btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì địt =))))',
+    reply: 'Yêu thì yêu mà không yêu thì yêu =))))',
     mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Chiều chủ nhật, mình làm nháy nha.',
+    messDesc: 'Chiều chủ nhật, mình nch tiếp nha.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://www.facebook.com/kien15042003/' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
